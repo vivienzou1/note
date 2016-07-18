8210d8898e34056f0708e007c419d6cb88a90314
liguibing <lyb411@126.com>
2016-07-18 19:01:49 +0800

    Create install.go

---
+++ go/install.go
@@ -0,0 +1,15 @@
+
+### 安装，直接解压即可
+  tar -zxvf https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
+  mv go /usr/local/
+
+### go 环境变量
+  export PATH=$PATH:/usr/local/go/bin
+  export GOROOT=/usr/local/go
+
+### beego框架环境变量设置
+  export GOPATH=/home/wwwroot/open/go/beego
+  export bee=/home/wwwroot/open/go/beego/bin
+
+###导入环境变量
+export PATH=$PATH:$GOROOT/bin:$GOPATH:$bee

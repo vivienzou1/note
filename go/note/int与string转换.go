eddb8dd028caf82215385e462e6ebb554bc73dc4
小金牛 <lyb411@126.com>
2017-04-14 16:00:10 +0800

    Create int与string转换.go

---
+++ go/note/int与string转换.go
@@ -0,0 +1,12 @@
+//将string转换为int
+/*var numStr string = "89"
+if v, err := strconv.Atoi(numStr); err == nil {
+	fmt.Printf("i=%d, type: %T\n", v, v)
+}
+fmt.Println(newStr)*/
+
+//将ascll码转换为对应的string
+//tmp := string(str[i])
+
+//将int转换为string
+//newStr = newStr + strconv.Itoa(num)

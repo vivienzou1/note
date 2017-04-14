3709c4b30f16162a19e1c0a5c4b5224e4c3d8edf
小金牛 <lyb411@126.com>
2017-04-14 16:44:03 +0800

    Create 变量.go

---
+++ go/note/变量.go
@@ -0,0 +1,18 @@
+//变量
+func variate() {
+	/*
+		       var a,b,c int = 1,3,4
+		       a,b,c := 1,2,3
+
+			   var x, y *int
+			   var xxx, yyy, zzz int
+			   var address string = "北京中关村"
+			   var num int
+			   var aa bool = false
+			   var (
+			       aaaa int
+			       bbbb bool
+			       strs string = "这是变量"
+			   )
+	*/
+}

e41d404fe627ca6e3752a47b0dea0b0a63a2ba23
小金牛 <lyb411@126.com>
2017-04-14 15:54:58 +0800

    Create 时间和日期.go

---
+++ go/note/时间和日期.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+
+	//secs := t.Unix()          // 秒
+	//nanos := t.UnixNano()     // 纳秒
+	//millis := nanos / 1000000 // 毫秒
+
+	//标准日期输出
+	fmt.Printf("日期：%4d-%02d-%02d %02d:%02d:%02d\n", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+
+	//格式化输出，输出当前标准时间
+	fmt.Println(t.Format("2006-01-02 15:04:05"))
+
+	//获取时间戳
+	timestamp := time.Now().Unix()
+	fmt.Println(timestamp)
+
+	//获取当前的标准日期格式
+	tm := time.Unix(timestamp, 0)
+	fmt.Println(tm.Format("2006-01-02 15:04:05")) //时间模板，这是个奇葩,必须是这个时间点(2006-01-02 15:04:05),go的诞生日
+
+	//将时间戳转换为标准日期格式
+	str_time := time.Unix(1492156262, 0).Format("2006-01-02 15:04:05")
+	fmt.Println(str_time)
+
+	//将标准日期转为时间戳，第一个参数是模板格式(值不能变)，第二个是要转换的时间字符串
+	tm2, _ := time.Parse("2006-01-02 15:04:05", "2017-04-14 15:51:02")
+	fmt.Println(tm2.Unix())
+
+}

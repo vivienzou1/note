b6db0c51ecf3d88a82b78a66809212a23d1df3d9
小金牛 <lyb411@126.com>
2017-04-14 15:31:42 +0800

    Create 字符串处理.go

---
+++ go/note/字符串处理.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func init() {
+
+}
+
+func main() {
+	//判断字符串是否以hell开头,结尾函数用：HasSuffix
+	str := strings.HasPrefix("hello word", "hell")
+	fmt.Printf("%v\n", str)
+
+	//判断字符串是否包含 CDF
+	str2 := strings.Contains("ABCDFDFSD", "CDF")
+	fmt.Println(str2)
+
+	//判断字符串CD在ABCDFDFSD第一次出现的位置
+	str3 := strings.Index("ABCDFDFSDCD", "CD")
+	fmt.Println(str3)
+
+	//判断字符串CD在ABCDFDFSD最后一次出现的位置
+	str4 := strings.LastIndex("ABCDFDFSDCD", "CD")
+	fmt.Println(str4)
+
+	//替换字符串,1:替换一次，-1：替换所有
+	str5 := strings.Replace("ABCDFDFSDCD", "CD", "这是替换后的新字符串", -1)
+	fmt.Println(str5)
+
+	//统计字符串出现次数
+	str6 := strings.Count("ABCDFDFSDCD", "CD")
+	fmt.Println(str6)
+
+	//全部转换为小写
+	str7 := strings.ToLower("ABCCDklEF")
+	fmt.Println(str7)
+
+	//全部转换为大写
+	str8 := strings.ToUpper("ABCCDklEFxyz")
+	fmt.Println(str8)
+
+	//删除字符串开头和结尾的空白符号
+	str9 := strings.TrimSpace("  ABCD  ")
+	fmt.Println(str9)
+
+	//删除开头和结尾指定字符,还可以只删除开头或结尾指定字符，用TrimLeft 或者 TrimRight
+	str10 := strings.Trim("  ABCD  ", " ")
+	fmt.Println(str10)
+
+	//将字符串分割成切片
+	str11 := strings.Split("ABC CDkl EFxyz", " ")
+	fmt.Println(str11)
+
+	str12 := strings.Split("[ABC CDkl EFxyz]", ",")
+	fmt.Println(str12)
+}

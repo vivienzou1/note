3b7b55c8071e60596082ed62baa721407c78317c
小金牛 <lyb411@126.com>
2018-01-19 16:11:24 +0800

    Update goroutine.go

--- go/goroutine.go
+++ go/goroutine.go
@@ -37,3 +37,49 @@ func main() {
 	fmt.Scanln(&input)
 	fmt.Println(total_tickets, "done") //退出时打印还有多少票
 }
+
+
+/**
+package main
+
+import "fmt"
+import "time"
+import "math/rand"
+
+var total_tickets int32 = 10
+
+func sell_tickets(i int) {
+	for {
+		if total_tickets > 0 { //如果有票就卖
+			time.Sleep(time.Duration(rand.Intn(5)) * time.Millisecond)
+			total_tickets-- //卖一张票
+			fmt.Println("id:", i, "  ticket:", total_tickets)
+		} else {
+			break
+		}
+	}
+}
+
+func main() {
+
+	//下面这种会卖超
+	/*for i := 0; i < 5; i++ { //并发5个goroutine来卖票
+	     go sell_tickets(i)
+	}*/
+
+	//并发50个goroutine来卖票
+	go func() {
+		for i := 0; i < 50; i++ {
+			sell_tickets(i)
+		}
+	}()
+
+	//等待线程执行完
+	var input string
+	fmt.Scanln(&input)
+
+	//退出时打印还有多少票
+	fmt.Println(total_tickets, "done")
+}
+
+**/
